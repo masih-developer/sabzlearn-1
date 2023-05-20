35174b2224f64446102cf1d9e2e1737817422338
masih abedini <masih7816@gmail.com>
2023-05-21 01:13:43 +0330

    new configure tailwind in config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,41 @@ export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     darkMode: "class",
     theme: {
+        screens: {
+            sm: "576px",
+            md: "768px",
+            lg: "992px",
+            xl: "1200px",
+            "2xl": "1400px",
+        },
         container: {
             center: true,
             padding: "1rem",
+            screens: {
+                sm: "540px",
+                md: "720px",
+                lg: "960px",
+                xl: "1140px",
+                "2xl": "1320px",
+            },
         },
-        extend: {},
+        fontFamily: {
+            IRANSans: "IRANSans",
+            "IRANSans-Medium": "IRANSans-Medium",
+            "IRANSans-Bold": "IRANSans-Bold",
+        },
+        // extends
+        extend: {
+            fontSize: {
+                md: "0.9375rem",
+            },
+            colors: {
+                "primary-color": "#2bce56",
+                "dark-color": "#464749",
+                "blue-hover": "#1e83f0",
+                "grey-color": "#f0f2f7",
+            },
+        },
     },
     plugins: [],
 };
