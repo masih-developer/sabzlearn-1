fdca71f9c6c22eabf381ec79b09c7c2367e66748
masih abedini <masih7816@gmail.com>
2023-05-26 00:39:39 +0330

    add NotFound page to routes.js

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,12 +2,14 @@ import Home from "./pages/Home";
 import ArticleInfo from "./pages/ArticleInfo";
 import Category from "./pages/Category";
 import CourseInfo from "./pages/CourseInfo";
+import NotFound from "./pages/NotFound";
 
 const routes = [
     { path: "/", element: <Home /> },
     { path: "/article-info/:articleName", element: <ArticleInfo /> },
     { path: "/category-info/:categoryName", element: <Category /> },
     { path: "/course-info/:courseName", element: <CourseInfo /> },
+    { path: "*", element: <NotFound /> },
 ];
 
 export default routes;
