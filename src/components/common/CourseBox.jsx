dd3834393525b1ec8199447d7b1de20aedd4e9e0
masih abedini <masih7816@gmail.com>
2023-05-30 22:14:39 +0330

    refcator CourseBox component

--- src/components/common/CourseBox.jsx
+++ src/components/common/CourseBox.jsx
@@ -1,25 +1,29 @@
 import { FaArrowLeft, FaChalkboardTeacher, FaRegStar, FaStar, FaUserFriends } from "react-icons/fa";
 
-const CourseBox = ({ img, title, teacher, studentCount, price }) => {
+const CourseBox = ({ img, title, teacher, studentCount, price, hoverEffect }) => {
     return (
-        <div className="flex flex-col bg-white shadow-[0_0_19px_rgba(168,172,185,0.3)] rounded-lg overflow-hidden select-none">
+        <div
+            className={`flex select-none flex-col overflow-hidden rounded-lg bg-white shadow-[0_0_19px_rgba(168,172,185,0.3)] ${
+                hoverEffect ? "duration-300 hover:-translate-y-2" : ""
+            }`}
+        >
             <a href="" className="block w-full">
                 <img src={img} alt="" className="w-full" />
             </a>
-            <div className="py-3 px-2">
+            <div className="px-2 py-3">
                 <a href="#">
                     <h4 className="text-dark-color duration-300 hover:text-blue-hover">{title}</h4>
                 </a>
-                <div className="flex justify-between items-center mt-3">
-                    <div className="flex justify-center items-center text-[#6c757d] gap-1">
+                <div className="mt-3 flex items-center justify-between">
+                    <div className="flex items-center justify-center gap-1 text-[#6c757d]">
                         <FaChalkboardTeacher className="text-xl" />
                         <a href="#">
-                            <p className="text-xs duration-300 hover:text-blue-hover font-IRANSans-Medium">
+                            <p className="font-IRANSans-Medium text-xs duration-300 hover:text-blue-hover">
                                 {teacher}
                             </p>
                         </a>
                     </div>
-                    <div className="flex justify-center items-center gap-[2px] text-lg text-[#f9a134]">
+                    <div className="flex items-center justify-center gap-[2px] text-lg text-[#f9a134]">
                         <FaRegStar />
                         <FaStar />
                         <FaStar />
@@ -27,18 +31,18 @@ const CourseBox = ({ img, title, teacher, studentCount, price }) => {
                         <FaStar />
                     </div>
                 </div>
-                <div className="flex justify-between items-center mt-3">
-                    <div className="flex justify-center items-center text-[#6c757d] gap-1">
+                <div className="mt-3 flex items-center justify-between">
+                    <div className="flex items-center justify-center gap-1 text-[#6c757d]">
                         <FaUserFriends className="text-xl" />
                         <p className="text-sm">{studentCount}</p>
                     </div>
                     <span className="text-lg text-[#9c9c9c]">{price}</span>
                 </div>
             </div>
-            <div className="flex justify-center items-center py-3 border-t-[#e8e8e8] border-t-2">
+            <div className="flex items-center justify-center border-t-2 border-t-[#e8e8e8] py-3">
                 <a
                     href=""
-                    className="text-primary-color flex justify-center items-center gap-2 duration-300 hover:text-blue-hover font-IRANSans-Medium"
+                    className="flex items-center justify-center gap-2 font-IRANSans-Medium text-primary-color duration-300 hover:text-blue-hover"
                 >
                     مشاهده اطلاعات
                     <FaArrowLeft />
