bf22aa1c8e656315516cf07142a2940681709068
masih abedini <masih7816@gmail.com>
2023-06-11 22:05:36 +0330

    add AuthProvider and Toaster wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,21 @@ import routes from "./routes";
 import Header from "./components/common/Header/Header";
 import SidebarProvider from "./context/SidebarProvider";
 import Footer from "./components/common/Footer";
-
+import AuthProvider from "./context/AuthProvider";
+import { Toaster } from "react-hot-toast";
 const App = () => {
     const router = useRoutes(routes);
 
     return (
         <>
-            <SidebarProvider>
-                <Header />
-                <main>{router}</main>
-                <Footer />
-            </SidebarProvider>
+            <AuthProvider>
+                <SidebarProvider>
+                    <Header />
+                    <Toaster position="top-center" reverseOrder={false} />
+                    <main>{router}</main>
+                    <Footer />
+                </SidebarProvider>
+            </AuthProvider>
         </>
     );
 };
