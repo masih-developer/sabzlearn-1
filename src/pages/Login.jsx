e3bf81196ea87bab43d988e0b9409d2f3f302183
masih abedini <masih7816@gmail.com>
2023-06-11 19:35:21 +0330

    creae Register-Login pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,76 @@
 import { FaLockOpen, FaUser } from "react-icons/fa";
 import { CgLogIn } from "react-icons/cg";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import AppInput from "../components/common/AppInput";
+import Button from "../components/common/Button";
+import { maxValidator, minValidator, requiredValidator } from "../validators/rules";
+import useForm from "../hooks/useForm";
+import toast from "react-hot-toast";
+import { useContext, useState } from "react";
+import { AuthContext } from "../context/AuthProvider";
+import ReCAPTCHA from "react-google-recaptcha";
 
 const Login = () => {
+    const [isLoadingLogginBtn, setIsLoadingLogginBtn] = useState(false);
+    const authContext = useContext(AuthContext);
+    const [isVerifyGoogleRecaptcha, setIsVerifyGoogleRecaptcha] = useState(false);
+    const navigate = useNavigate();
+    const [formState, onInputHandler] = useForm(
+        {
+            username: {
+                value: "",
+                isValid: false,
+            },
+            password: {
+                value: "",
+                isValid: false,
+            },
+        },
+        false
+    );
+
+    const userLoginHandler = (e) => {
+        e.preventDefault();
+        const userData = {
+            identifier: formState.inputs.username.value,
+            password: formState.inputs.password.value,
+        };
+
+        setIsLoadingLogginBtn(true);
+        fetch("http://localhost:4000/v1/auth/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(userData),
+        })
+            .then(async (res) => {
+                if (!res.ok) {
+                    toast.error("هیچ کاربری با این ایمیل یا نام کاربری وجود ندارد.");
+                    return res.text().then((text) => {
+                        throw new Error(text);
+                    });
+                }
+                return res.json();
+            })
+            .then((result) => {
+                toast.success("به حساب کاربری خود خوش آمدید.");
+                return result;
+            })
+            .then((value) => {
+                authContext.login(value.accessToken);
+                navigate("/");
+            })
+            .catch((err) => {
+                console.error(err);
+            })
+            .finally(() => setIsLoadingLogginBtn(false));
+    };
+
+    const onChangeHandler = () => {
+        setIsVerifyGoogleRecaptcha(true);
+    };
+
     return (
         <div className="mt-20 lg:mt-10">
             <div className="container">
@@ -22,30 +90,61 @@ const Login = () => {
                             ثبت نام
                         </Link>
                     </div>
-                    <form action="#" className="flex flex-col gap-4">
-                        <div className="flex items-center justify-between rounded-md border border-[#dcdcdc] p-2">
-                            <input
+                    <form
+                        action="#"
+                        className="flex flex-col gap-4"
+                        onSubmit={(e) => userLoginHandler(e)}
+                    >
+                        <div className="relative flex items-center justify-between">
+                            <AppInput
                                 type="text"
-                                className="w-full border-0 pl-2 text-md text-dark-color outline-0 placeholder:text-sm"
+                                id="username"
+                                className="w-full rounded-md border border-[#dcdcdc] p-2 pl-10 text-md text-dark-color outline-0 placeholder:text-sm"
                                 placeholder="نام کاربری یا آدرس ایمیل"
+                                elem="input"
+                                validations={[
+                                    requiredValidator(),
+                                    minValidator(8),
+                                    maxValidator(20),
+                                ]}
+                                onInputHandler={onInputHandler}
                             />
-                            <FaUser className="shrink-0 text-xl text-[#8d8d8d]" />
+                            <FaUser className="absolute bottom-0 left-2 top-0 m-auto shrink-0 text-xl text-[#8d8d8d]" />
                         </div>
-                        <div className="flex items-center justify-between rounded-md border border-[#dcdcdc] p-2">
-                            <input
+                        <div className="relative flex items-center justify-between">
+                            <AppInput
                                 type="password"
-                                className="w-full border-0 pl-2 text-md text-dark-color outline-0 placeholder:text-sm"
+                                id="password"
+                                className="w-full rounded-md border border-[#dcdcdc] p-2 pl-10 text-md text-dark-color outline-0 placeholder:text-sm"
                                 placeholder="رمز عبور"
+                                elem="input"
+                                validations={[
+                                    requiredValidator(),
+                                    minValidator(8),
+                                    maxValidator(18),
+                                ]}
+                                onInputHandler={onInputHandler}
                             />
-                            <FaLockOpen className="shrink-0 text-xl text-[#8d8d8d]" />
+                            <FaLockOpen className="absolute bottom-0 left-2 top-0 m-auto shrink-0 text-xl text-[#8d8d8d]" />
                         </div>
-                        <button
-                            className="relative flex items-center justify-center rounded-md bg-primary-color p-2.5 text-white"
+                        <div className="">
+                            <ReCAPTCHA
+                                sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
+                                onChange={onChangeHandler}
+                            />
+                        </div>
+                        <Button
+                            className="relative flex items-center justify-center rounded-md bg-primary-color p-2.5 text-white disabled:opacity-70"
                             type="submit"
+                            disabled={!formState.isFormValid || !isVerifyGoogleRecaptcha}
                         >
-                            ورود
+                            {isLoadingLogginBtn ? (
+                                <span className="block h-6 w-6 animate-spin rounded-full border-[3px] border-green-100 border-t-primary-color"></span>
+                            ) : (
+                                "ورود"
+                            )}
                             <CgLogIn className="absolute bottom-0 right-2.5 top-0 m-auto text-xl" />
-                        </button>
+                        </Button>
                         <div className="flex flex-col items-start justify-between gap-1 text-sm text-dark-color sm:flex-row sm:gap-0">
                             <div className="flex select-none items-center gap-1 text-xs sm:text-sm">
                                 <input type="checkbox" id="checkbox" className="" />
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,87 @@
-import { FaEnvelope, FaLockOpen, FaUser, FaUserPlus } from "react-icons/fa";
+import { FaEnvelope, FaLockOpen, FaUser, FaUserEdit, FaUserPlus } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import AppInput from "../components/common/AppInput";
+import Button from "../components/common/Button";
+import {
+    confirmPassValidator,
+    emailValidator,
+    maxValidator,
+    minValidator,
+    phoneNumberValidator,
+    requiredValidator,
+} from "../validators/rules";
+import useForm from "../hooks/useForm";
+import { AuthContext } from "../context/AuthProvider";
+import { useContext } from "react";
+import { toast } from "react-hot-toast";
 
 const Register = () => {
+    const authContext = useContext(AuthContext);
+    const [formState, onInputHandler] = useForm(
+        {
+            name: {
+                value: "",
+                isValid: false,
+            },
+            username: {
+                value: "",
+                isValid: false,
+            },
+            email: {
+                value: "",
+                isValid: false,
+            },
+            password: {
+                value: "",
+                isValid: false,
+            },
+            phone: {
+                value: "",
+                isValid: false,
+            },
+            confirmPassword: {
+                value: "",
+                isValid: false,
+            },
+        },
+        false
+    );
+
+    const RegisterUserHandler = (e) => {
+        e.preventDefault();
+        if (formState.isFormValid) {
+            const newUser = {
+                username: formState.inputs.username.value,
+                email: formState.inputs.email.value,
+                password: formState.inputs.password.value,
+                confirmPassword: formState.inputs.confirmPassword.value,
+                name: formState.inputs.name.value,
+                phone: formState.inputs.phone.value,
+            };
+
+            fetch("http://localhost:4000/v1/auth/register", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(newUser),
+            })
+                .then((res) => res.json())
+                .then(() => {
+                    toast.success("حساب کاربری با موفقیت ساخته شد.");
+                })
+                .then((result) => {
+                    if (result.accessToken) {
+                        authContext.login(result.accessToken);
+                    }
+                })
+                .catch((err) => {
+                    console.log(err);
+                    toast.error("خطا در انجام عملیات ساخت حساب!");
+                });
+        }
+    };
+
     return (
         <div className="mt-20 lg:mt-10">
             <div className="container">
@@ -15,44 +95,114 @@ const Register = () => {
                     <div className="my-5 mb-5 flex items-center justify-center gap-3 rounded-md bg-grey-color p-3 text-sm text-dark-color">
                         <span className="text-sm">قبلا ثبت نام کرده اید؟</span>
                         <Link
-                            to="/register"
+                            to="/login"
                             className="rounded-md bg-[#666666] px-2 py-1 text-xs text-white"
                         >
                             وارد شوید
                         </Link>
                     </div>
-                    <form action="#" className="flex flex-col gap-4">
-                        <div className="flex items-center justify-between rounded-md border border-[#dcdcdc] p-2">
-                            <input
+                    <form
+                        action="#"
+                        className="flex flex-col gap-4"
+                        onSubmit={(e) => RegisterUserHandler(e)}
+                    >
+                        <div className="relative flex items-center justify-between">
+                            <AppInput
                                 type="text"
-                                className="w-full border-0 pl-2 text-md text-dark-color outline-0 placeholder:text-sm"
+                                id="name"
+                                className="w-full rounded-md border border-[#dcdcdc] p-2 pl-10 text-md text-dark-color outline-0 placeholder:text-sm"
+                                placeholder="نام و نام خانوادگی"
+                                elem="input"
+                                validations={[
+                                    requiredValidator(),
+                                    minValidator(6),
+                                    maxValidator(20),
+                                ]}
+                                onInputHandler={onInputHandler}
+                            />
+                            <FaUserEdit className="absolute bottom-0 left-2 top-0 m-auto shrink-0 text-xl text-[#8d8d8d]" />
+                        </div>
+                        <div className="relative flex items-center justify-between">
+                            <AppInput
+                                type="text"
+                                id="username"
+                                className="w-full rounded-md border border-[#dcdcdc] p-2 pl-10 text-md text-dark-color outline-0 placeholder:text-sm"
                                 placeholder="نام کاربری"
+                                elem="input"
+                                validations={[
+                                    requiredValidator(),
+                                    minValidator(8),
+                                    maxValidator(20),
+                                ]}
+                                onInputHandler={onInputHandler}
                             />
-                            <FaUser className="shrink-0 text-xl text-[#8d8d8d]" />
+                            <FaUser className="absolute bottom-0 left-2 top-0 m-auto shrink-0 text-xl text-[#8d8d8d]" />
                         </div>
-                        <div className="flex items-center justify-between rounded-md border border-[#dcdcdc] p-2">
-                            <input
+                        <div className="relative flex items-center justify-between">
+                            <AppInput
                                 type="text"
-                                className="w-full border-0 pl-2 text-md text-dark-color outline-0 placeholder:text-sm"
+                                id="email"
+                                className="w-full rounded-md border border-[#dcdcdc] p-2 pl-10 text-md text-dark-color outline-0 placeholder:text-sm"
                                 placeholder="آدرس ایمیل"
+                                elem="input"
+                                validations={[emailValidator()]}
+                                onInputHandler={onInputHandler}
                             />
-                            <FaEnvelope className="shrink-0 text-xl text-[#8d8d8d]" />
+                            <FaEnvelope className="absolute bottom-0 left-2 top-0 m-auto shrink-0 text-xl text-[#8d8d8d]" />
                         </div>
-                        <div className="flex items-center justify-between rounded-md border border-[#dcdcdc] p-2">
-                            <input
+                        <div className="relative flex items-center justify-between">
+                            <AppInput
                                 type="password"
-                                className="w-full border-0 pl-2 text-md text-dark-color outline-0 placeholder:text-sm"
+                                id="password"
+                                className="w-full rounded-md border border-[#dcdcdc] p-2 pl-10 text-md text-dark-color outline-0 placeholder:text-sm"
                                 placeholder="رمز عبور"
+                                elem="input"
+                                validations={[
+                                    requiredValidator(),
+                                    minValidator(8),
+                                    maxValidator(20),
+                                ]}
+                                onInputHandler={onInputHandler}
                             />
-                            <FaLockOpen className="shrink-0 text-xl text-[#8d8d8d]" />
+                            <FaLockOpen className="absolute bottom-0 left-2 top-0 m-auto shrink-0 text-xl text-[#8d8d8d]" />
                         </div>
-                        <button
-                            className="relative flex items-center justify-center rounded-md bg-primary-color p-2.5 text-white"
+                        <div className="relative flex items-center justify-between">
+                            <AppInput
+                                type="password"
+                                id="confirmPassword"
+                                className="w-full rounded-md border border-[#dcdcdc] p-2 pl-10 text-md text-dark-color outline-0 placeholder:text-sm"
+                                placeholder="تکرار رمز عبور"
+                                elem="input"
+                                validations={[
+                                    requiredValidator(),
+                                    minValidator(8),
+                                    maxValidator(20),
+                                    confirmPassValidator(formState.inputs.password.value),
+                                ]}
+                                onInputHandler={onInputHandler}
+                            />
+                            <FaLockOpen className="absolute bottom-0 left-2 top-0 m-auto shrink-0 text-xl text-[#8d8d8d]" />
+                        </div>
+                        <div className="relative flex items-center justify-between">
+                            <AppInput
+                                type="text"
+                                id="phone"
+                                className="w-full rounded-md border border-[#dcdcdc] p-2 pl-10 text-md text-dark-color outline-0 placeholder:text-sm"
+                                placeholder="شماره تلفن همراه"
+                                elem="input"
+                                validations={[phoneNumberValidator()]}
+                                onInputHandler={onInputHandler}
+                            />
+                            <FaLockOpen className="absolute bottom-0 left-2 top-0 m-auto shrink-0 text-xl text-[#8d8d8d]" />
+                        </div>
+                        <Button
+                            className="relative flex items-center justify-center rounded-md bg-primary-color p-2.5 text-white disabled:opacity-70"
                             type="submit"
+                            disabled={!formState.isFormValid}
                         >
                             عضویت
                             <FaUserPlus className="absolute bottom-0 right-2.5 top-0 m-auto text-xl" />
-                        </button>
+                        </Button>
                     </form>
                     <div className="mt-7">
                         <span className="text-sm text-dark-color">سلام کاربر محترم:</span>
