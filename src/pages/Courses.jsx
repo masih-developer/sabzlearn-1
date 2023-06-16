bdf719be4ea78cc875daa9c664db07264f39f6e8
masih abedini <masih7816@gmail.com>
2023-06-16 23:02:20 +0330

    add Pagination to CourseInfo

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,8 +1,20 @@
-import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 import Breadcrumb from "../components/common/Breadcrumb";
 import CourseBox from "../components/common/CourseBox";
+import { useEffect, useState } from "react";
+import Pagination from "../components/common/Pagination";
 
 const Courses = () => {
+    const [allCourses, setAllCourses] = useState([]);
+    const [paginatedItems, setPaginatedItems] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:4000/v1/courses")
+            .then((res) => res.json())
+            .then((result) => {
+                setAllCourses(result);
+            });
+    }, []);
+
     return (
         <div>
             <div className="container">
@@ -12,83 +24,36 @@ const Courses = () => {
                         { id: 2, title: "تمامی دوره ها", path: "/courses" },
                     ]}
                 />
-                <div className="mt-5 grid grid-cols-1 gap-x-4 gap-y-10 sm:grid-cols-2 lg:grid-cols-3">
-                    <CourseBox
-                        hoverEffect
-                        img="/images/courses/fareelancer.png"
-                        title="دوره پروژه های فریلنسری"
-                        teacher="رضا دولتی"
-                        studentCount="1000"
-                        price={2_000_000}
-                    />
-                    <CourseBox
-                        hoverEffect
-                        img="/images/courses/jango.png"
-                        title="دوره پروژه های فریلنسری"
-                        teacher="رضا دولتی"
-                        studentCount="1000"
-                        price={2_000_000}
-                    />
-                    <CourseBox
-                        hoverEffect
-                        img="/images/courses/js_project.png"
-                        title="دوره پروژه های فریلنسری"
-                        teacher="رضا دولتی"
-                        studentCount="1000"
-                        price={2_000_000}
-                    />
-                    <CourseBox
-                        hoverEffect
-                        img="/images/courses/js_project.png"
-                        title="دوره پروژه های فریلنسری"
-                        teacher="رضا دولتی"
-                        studentCount="1000"
-                        price={2_000_000}
-                    />
-                    <CourseBox
-                        hoverEffect
-                        img="/images/courses/js_project.png"
-                        title="دوره پروژه های فریلنسری"
-                        teacher="رضا دولتی"
-                        studentCount="1000"
-                        price={2_000_000}
-                    />
-                    <CourseBox
-                        hoverEffect
-                        img="/images/courses/js_project.png"
-                        title="دوره پروژه های فریلنسری"
-                        teacher="رضا دولتی"
-                        studentCount="1000"
-                        price={2_000_000}
-                    />
-                    <CourseBox
-                        hoverEffect
-                        img="/images/courses/js_project.png"
-                        title="دوره پروژه های فریلنسری"
-                        teacher="رضا دولتی"
-                        studentCount="1000"
-                        price={2_000_000}
-                    />
-                </div>
-                <div className="mt-10 flex w-full items-center justify-center gap-2">
-                    <button className="flex h-10 w-10 items-center justify-center rounded-md bg-[#f0f0f1] text-dark-color duration-300 hover:bg-primary-color hover:text-white">
-                        <FaArrowRight />
-                    </button>
-                    <ul className="flex items-center justify-center gap-2">
-                        <li className="flex h-10 w-10 cursor-pointer items-center justify-center rounded-md bg-[#f0f0f1] text-dark-color duration-300 hover:bg-primary-color hover:text-white">
-                            1
-                        </li>
-                        <li className="flex h-10 w-10 cursor-pointer items-center justify-center rounded-md bg-[#f0f0f1] text-dark-color duration-300 hover:bg-primary-color hover:text-white">
-                            2
-                        </li>
-                        <li className="flex h-10 w-10 cursor-pointer items-center justify-center rounded-md bg-[#f0f0f1] text-dark-color duration-300 hover:bg-primary-color hover:text-white">
-                            3
-                        </li>
-                    </ul>
-                    <button className="flex h-10 w-10 items-center justify-center rounded-md bg-[#f0f0f1] text-dark-color duration-300 hover:bg-primary-color hover:text-white">
-                        <FaArrowLeft />
-                    </button>
-                </div>
+                {allCourses.length ? (
+                    <>
+                        <div className="mt-5 grid grid-cols-1 gap-x-4 gap-y-10 sm:grid-cols-2 lg:grid-cols-3">
+                            {paginatedItems.map((course) => (
+                                <CourseBox
+                                    key={course._id}
+                                    hoverEffect
+                                    img={course.cover}
+                                    title={course.name}
+                                    teacher={course.creator}
+                                    studentCount={course.registers}
+                                    price={course.price}
+                                    path={course.shortName}
+                                />
+                            ))}
+                        </div>
+                        <Pagination
+                            items={allCourses}
+                            itemsCount={6}
+                            pathname="/courses"
+                            setPaginatedItems={setPaginatedItems}
+                        />
+                    </>
+                ) : (
+                    <div className="texty mt-5 flex h-20 w-full items-center justify-center rounded-lg bg-yellow-100 text-yellow-500">
+                        <h2 className="font-IRANSans-Medium text-xl">
+                            هیچ دوره ای برای نمایش وجود ندارد.
+                        </h2>
+                    </div>
+                )}
             </div>
         </div>
     );
