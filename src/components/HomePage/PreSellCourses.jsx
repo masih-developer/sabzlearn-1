e02d0db22bd682d5594d6f8601fdcf929a4f3651
masih abedini <masih7816@gmail.com>
2023-06-19 01:15:36 +0330

    PreSellCourses insted last component

---
+++ src/components/HomePage/PreSellCourses.jsx
@@ -0,0 +1,66 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+import SectionTitle from "../common/SectionTitle";
+import CourseBox from "../common/CourseBox";
+import { useEffect, useState } from "react";
+
+const PreSaleCourses = () => {
+    const [courses, setCourses] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:4000/v1/courses/presell")
+            .then((res) => res.json())
+            .then((result) => {
+                setCourses(result);
+            });
+    }, []);
+
+    return (
+        <section className="mt-20">
+            <div className="container">
+                <SectionTitle title="دوره های در حال پیش فروش" />
+                <div className="mt-10" style={{ direction: "ltr" }}>
+                    <Swiper
+                        speed={800}
+                        dir="rtl"
+                        spaceBetween={20}
+                        slidesPerView={3}
+                        breakpoints={{
+                            0: {
+                                slidesPerView: 1,
+                                spaceBetween: 10,
+                            },
+                            576: {
+                                slidesPerView: 2,
+                                spaceBetween: 20,
+                            },
+                            992: {
+                                slidesPerView: 3,
+                                spaceBetween: 40,
+                            },
+                        }}
+                        loop={true}
+                        className="py-1"
+                    >
+                        {courses.map((course) => (
+                            <SwiperSlide key={course._id}>
+                                <CourseBox
+                                    img={course.cover}
+                                    title={course.name}
+                                    teacher={course.creator}
+                                    rate={5}
+                                    studentCount={course.registers}
+                                    price={course.price}
+                                    path={course.shortName}
+                                />
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default PreSaleCourses;
