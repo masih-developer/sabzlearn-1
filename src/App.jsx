737d82fd5d609f39b7efdffb68b1212dbc460f98
masih abedini <masih7816@gmail.com>
2023-06-22 23:23:38 +0330

    use pathname to recognize routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRoutes } from "react-router-dom";
+import { useLocation, useRoutes } from "react-router-dom";
 import routes from "./routes";
 import Header from "./components/common/Header/Header";
 import SidebarProvider from "./context/SidebarProvider";
@@ -6,18 +6,35 @@ import Footer from "./components/common/Footer";
 import AuthProvider from "./context/AuthProvider";
 import { Toaster } from "react-hot-toast";
 import ScrollToTop from "./components/common/ScrollToTop";
+import { useEffect, useState } from "react";
 const App = () => {
     const router = useRoutes(routes);
+    const [showComponents, setShowComponents] = useState(true);
+
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        setShowComponents(!pathname.includes("p-admin"));
+    }, [pathname]);
 
     return (
         <>
             <AuthProvider>
                 <SidebarProvider>
                     <ScrollToTop>
-                        <Header />
-                        <Toaster position="top-center" reverseOrder={false} />
-                        <main>{router}</main>
-                        <Footer />
+                        {showComponents ? (
+                            <>
+                                <Header />
+                                <Toaster position="top-center" reverseOrder={false} />
+                                <main>{router}</main>
+                                <Footer />
+                            </>
+                        ) : (
+                            <>
+                                <Toaster position="top-center" reverseOrder={false} />
+                                <main>{router}</main>
+                            </>
+                        )}
                     </ScrollToTop>
                 </SidebarProvider>
             </AuthProvider>
