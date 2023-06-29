d22c7086dd6f47bb52d2e65715c76264934051e4
masih abedini <masih7816@gmail.com>
2023-06-30 00:11:43 +0330

    create DataTable component

--- src/components/AdminPanel/DataTable.jsx
+++ src/components/AdminPanel/DataTable.jsx
@@ -1,6 +1,8 @@
-const DataTable = ({ title, children }) => {
+const DataTable = ({ title, children, className }) => {
     return (
-        <div className="rounded-lg p-5 shadow-[0px_0px_20px_2px_rgba(0,0,0,0.1)]">
+        <div
+            className={`w-full rounded-lg p-5 shadow-[0px_0px_20px_2px_rgba(0,0,0,0.1)] ${className}`}
+        >
             <h3 className="mb-5 font-IRANSans-Medium text-lg text-dark-color">
                 لیست <span className="font-IRANSans-Bold text-blue-800">{title}</span>
             </h3>
