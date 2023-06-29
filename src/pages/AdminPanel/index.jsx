34f3629ed70e65709c8e3d7da801f98f4f7a54cb
masih abedini <masih7816@gmail.com>
2023-06-30 00:14:06 +0330

    refactor styles code

--- src/pages/AdminPanel/index.jsx
+++ src/pages/AdminPanel/index.jsx
@@ -6,9 +6,9 @@ const index = () => {
     return (
         <div className="relative flex">
             <Sidebar />
-            <div className="mr-auto w-[calc(100%-250px)]">
+            <div className="mr-auto w-full lg:w-[calc(100%-250px)]">
                 <Topbar />
-                <div className="container mt-[84px]">{<Outlet />}</div>
+                <div className="container mt-[84px] min-h-screen">{<Outlet />}</div>
             </div>
         </div>
     );
